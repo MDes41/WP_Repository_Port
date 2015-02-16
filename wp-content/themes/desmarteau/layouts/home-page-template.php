0c06eeb598f4bc3ec14db85078dd3448e1c97bb1
Matthew <MDes41@users.noreply.github.com>
2015-02-16 16:17:21 -0700

    First Commit
    First Commit

---
+++ wp-content/themes/desmarteau/layouts/home-page-template.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @package Portfolio
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		<div class="row">
+			<h1><?php the_field('portfolio_title'); ?></h1>
+			<h2><?php the_field('portfolio_summary');?></h2>
+		</div>
+		
+
+
+		<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
