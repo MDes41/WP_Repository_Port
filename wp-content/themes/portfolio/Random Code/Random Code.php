a21e4cc2f58a9f4ec66076a4d3e2114f6545377c
Matthew <MDes41@users.noreply.github.com>
2015-02-25 12:57:37 -0700

    Site Up and Working Starting Save
    Site Up and Working, Pre Development of Portfolio Site,

---
+++ wp-content/themes/portfolio/Random Code/Random Code.php
@@ -0,0 +1,16 @@
+
+			<div class="row">
+				<div class="column-fourth">
+					<img src="<?php the_field('home_feature_image_one'); ?>"; >	
+				</div>
+				<div class="column-fourth">
+					<img src="<?php the_field('home_feature_image_two'); ?>"; >	
+				</div>
+				<div class="column-fourth">
+					<img src="<?php the_field('home_feature_image_three'); ?>"; >	
+				</div>
+				<div class="column-fourth">
+					<img src="<?php the_field('home_feature_image_four'); ?>"; >	
+				</div>
+			</div>
+		
--- wp-content/themes/portfolio/layouts/home-page-template.php
+++ wp-content/themes/portfolio/layouts/home-page-template.php
@@ -12,15 +12,12 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-		<div class="row">
-			<div class="column-half">
-			
-			</div>	
+
 			<div class="column-half">
 				<h1><?php the_field('portfolio_title'); ?></h1>
 				<h2><?php the_field('portfolio_summary');?></h2>
 			</div>
-		</div>
+
 		
 
 
